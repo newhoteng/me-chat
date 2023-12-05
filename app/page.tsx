02fe53cf14a88c8e1b5ffd04a36cb0d674b76f54
Harriet Oteng <harriet.oteng@gmail.com>
2023-12-05 11:53:39 +0000

    Remove form border

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <p className="rounded-bl-none rounded-2xl w-3/4 p-2 text-chatonleft bg-leftchatbg">That sounds great. I&apos;d be happy with that.</p>
         <p className="rounded-2xl rounded-br-none w-3/4 p-2 relative ml-auto text-chatonright bg-white">Could you send over some pictures of your dog, please?</p>
       </div>
-      <form action="" className="w-full absolute bottom-0 left-0 border border-blue-600 p-4">
+      <form action="" className="w-full absolute bottom-0 left-0 p-4">
         <div className="relative rounded-full">
           <input placeholder="Present-Self says ..." type="text" className="h-[50px] w-full rounded-full pl-4 pr-[50px] focus:outline-none placeholder:text-placeholder placeholder:font-light"/>
           <button type="submit" className="absolute top-[7.5px] right-[7.5px] bg-submitbutton w-[35px] h-[35px] rounded-full flex items-center justify-center">
