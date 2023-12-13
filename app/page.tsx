dc7e36e5691d9ca0474a685bf145ad6199ad1c7d
Harriet Oteng <harriet.oteng@gmail.com>
2023-12-13 23:00:25 +0000

    Display diff placeholder depending on which self

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
+'use client';
 // import Image from 'next/image';
+import { useState } from 'react';
 import { RiSendPlaneFill } from "react-icons/ri";
 import Switch from './ui/switch';
 
 export default function Home() {
+  const [isFutureSelf, setIsFutureSelf] = useState<boolean>(false);
+
   return (
     <main className="relative bg-appbackground h-screen mx-auto px-4 pt-[100px]">
       <header className="fixed top-0 left-0 w-full h-[100px] flex items-center justify-center gap-6 bg-gradient-to-r from-lightviolet to-lightmagenta text-pixel2 text-lg font-medium">
         <span className="">Present-Self</span>
-        <Switch />
+        <Switch isFutureSelf={isFutureSelf} setIsFutureSelf={setIsFutureSelf} />
         <span>Future-Self</span>
       </header>
       <div className='flex flex-col gap-4 pt-2 pb-[82px] borde border-red-600'>
@@ -16,7 +20,11 @@ export default function Home() {
       </div>
       <form action="" className="w-full absolute bottom-0 left-0 p-4">
         <div className="relative rounded-full">
-          <input placeholder="Present-Self says ..." type="text" className="h-[50px] w-full rounded-full pl-4 pr-[50px] focus:outline-none placeholder:text-placeholder placeholder:font-light"/>
+          {isFutureSelf ? (
+            <input placeholder="Future-Self says ..." type="text" className="h-[50px] w-full rounded-full pl-4 pr-[50px] focus:outline-none placeholder:text-placeholder placeholder:font-light"/>
+          ) : (
+            <input placeholder="Present-Self says ..." type="text" className="h-[50px] w-full rounded-full pl-4 pr-[50px] focus:outline-none placeholder:text-placeholder placeholder:font-light"/>
+          )}
           <button type="submit" className="absolute top-[7.5px] right-[7.5px] bg-submitbutton w-[35px] h-[35px] rounded-full flex items-center justify-center">
             <RiSendPlaneFill className="text-white text-xl borde border-yellow-50" />
           </button>
--- app/ui/switch.tsx
+++ app/ui/switch.tsx
@@ -4,19 +4,19 @@ import React, { useState } from 'react';
 // false - present
 // true - future
 
-const Switch = () => {
-  const [toggle, setToggle] = useState<boolean>(false);
+const Switch = ({ isFutureSelf, setIsFutureSelf } ) => {
+  // const [isFutureSelf, setToggle] = useState<boolean>(false);
 
   return (
     <div
-      onClick={() => setToggle(!toggle)}
+      onClick={() => setIsFutureSelf(!isFutureSelf)}
       className={`flex h-6 w-12 cursor-pointer rounded-full p-[2px] ${
-        toggle ? "bg-white justify-end" : "bg-leftchatbg justify-start"
+        isFutureSelf ? "bg-white justify-end" : "bg-leftchatbg justify-start"
       } duration-300`}
     >
       <div
         className={`h-5 w-5 rounded-full ${
-          toggle ? "bg-chatonright" : "bg-chatonleft"
+          isFutureSelf ? "bg-chatonright" : "bg-chatonleft"
         }`}>
       </div>
     </div>
