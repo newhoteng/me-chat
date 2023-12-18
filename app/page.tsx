361b54c15d6b85d40cbeba7f7ef89e84e4d961d7
Harriet Oteng <harriet.oteng@gmail.com>
2023-12-18 19:42:10 +0000

    Install uuid

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@
 import { useEffect, useState } from 'react';
 import { RiSendPlaneFill } from "react-icons/ri";
 import Switch from './ui/switch';
+import { Message } from './lib/definitions';
+// import { v4 as uuidv4 } from 'uuid';
 
 export default function Home() {
   const [ isFutureSelf, setIsFutureSelf ] = useState<boolean>(false);
   const [ message, setMessage ] = useState<string>('');
-  const [ messagesArray, setMessagesArray ] = useState<string[]>([])
+  const [ messagesArray, setMessagesArray ] = useState<Message[]>([])
 
-  // const taskStorage = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
   const storedMessages = localStorage.getItem('messages');
 
   useEffect(() => {
@@ -22,6 +23,10 @@ export default function Home() {
     localStorage.setItem('messages', JSON.stringify(messagesArray))
   }, [messagesArray])
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
+
   return (
     <main className="relative bg-appbackground h-screen mx-auto px-4 pt-[100px]">
       <header className="fixed top-0 left-0 w-full h-[100px] flex items-center justify-center gap-6 bg-gradient-to-r from-lightviolet to-lightmagenta text-pixel2 text-lg font-medium">
@@ -33,10 +38,11 @@ export default function Home() {
         <p className="rounded-bl-none rounded-2xl w-3/4 p-2 text-chatonleft bg-leftchatbg" contentEditable="true">That sounds great. I&apos;d be happy with that.</p>
         <p className="rounded-2xl rounded-br-none w-3/4 p-2 relative ml-auto text-chatonright bg-white" contentEditable="true">Could you send over some pictures of your dog, please?</p>
       </div>
-      <form action="" className="w-full absolute bottom-0 left-0 p-4">
+      <form onSubmit={handleSubmit} action="" className="w-full absolute bottom-0 left-0 p-4">
         <div className="relative rounded-full">
           {isFutureSelf ? (
             <input
+              name="message"
               placeholder="Future-Self says..."
               type="text"
               className="h-[50px] w-full rounded-full pl-4 pr-[50px] focus:outline-none placeholder:text-placeholder placeholder:font-light"
@@ -45,6 +51,7 @@ export default function Home() {
             />
           ) : (
             <input
+              name="message"
               placeholder="Present-Self says..."
               type="text"
               className="h-[50px] w-full rounded-full pl-4 pr-[50px] focus:outline-none placeholder:text-placeholder placeholder:font-light"
