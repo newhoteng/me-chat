92015392a0a9adec5eef8cf2e29cf72cd08aaf94
Harriet Oteng <harriet.oteng@gmail.com>
2023-12-29 06:18:31 +0000

    Use doodle background

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const [ isFutureSelf, setIsFutureSelf ] = useState<boolean>(false);
 
   return (
-    <main className="relative bg-appbackground h-screen mx-auto px-4 pt-[100px]">
+    <main className="relative bg-doodle h-screen mx-auto px-4 pt-[100px]">
       <header className="fixed top-0 left-0 w-full h-[100px] flex items-center justify-center gap-6 bg-gradient-to-r from-lightviolet to-lightmagenta text-pixel2 text-lg font-medium">
         <Header isFutureSelf={isFutureSelf} setIsFutureSelf={setIsFutureSelf} />
       </header>
@@ -21,3 +21,5 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     </main>
   );
 }
+
+//  bg-appbackground
